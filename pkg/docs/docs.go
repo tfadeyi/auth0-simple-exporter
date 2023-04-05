61c6e62c7817907009e80047ad1c0b311c862d1e
Oluwole Fadeyi <16049411+tfadeyi@users.noreply.github.com>
2023-04-05 20:21:03 +0100

    Prepare for new tag v0.0.2
    Add new tag, v0.0.2
    
    Signed-off-by: Oluwole Fadeyi <16049411+tfadeyi@users.noreply.github.com>

--- pkg/docs/docs.go
+++ pkg/docs/docs.go
@@ -48,7 +48,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "0.0.1",
+	Version:          "0.0.2",
 	Host:             "localhost:9301",
 	BasePath:         "/",
 	Schemes:          []string{},
